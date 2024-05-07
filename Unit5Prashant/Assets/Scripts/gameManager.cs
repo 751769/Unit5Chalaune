9ef4857d5eb14c475729db1f505e3b3aa3a1b33d
Prashant <751769@student.dusd.net>
2024-05-07 15:20:25 -0700

    heheheha
    Starting Create Game Over Function

--- Unit5Prashant/Assets/Scripts/gameManager.cs
+++ Unit5Prashant/Assets/Scripts/gameManager.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class gameManager : MonoBehaviour
+
+public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    private int score;
     private float spawnRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+
+        gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -26,6 +35,13 @@ public class gameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+
         }
+    }
+    
+   public void UpdateScore (int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "score: " + score;
     }
 }
